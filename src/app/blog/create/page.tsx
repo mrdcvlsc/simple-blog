0be6447cf39aaf7f897b7ae5f804608a738c016e
mrdcvlsc <mrdcvlsc@gmail.com>
2026-01-13 00:19:02 +0800

    remove unused initial boiler plate code

--- src/app/blog/create/page.tsx
+++ src/app/blog/create/page.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image"
-import supabase from "@/app/_lib/_supabase_server_client"
-
 export default function Home() {
     return <>
         <h1>This is the blog CREATE page</h1>
-        <Image src="/next.svg" alt="test-image" width={100} height={100} />
     </>
 }
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,5 @@
-import Image from "next/image"
-
 export default function Home() {
     return <>
         <h1>This is the blog wall page</h1>
-        <Image src="/next.svg" alt="test-image" width={100} height={100} />
     </>
 }
