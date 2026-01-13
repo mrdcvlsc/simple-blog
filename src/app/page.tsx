2ed019af5cf1711d56f2f87c41c7ef0754f54a76
mrdcvlsc <mrdcvlsc@gmail.com>
2026-01-13 13:00:31 +0800

    fix landing page links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import Link from "next/link"
 export default function LandingPage() {
     return <>
         <h1>Hello, Next.js THIS IS THE LANDING PAGE!</h1>
-        <Link href={'/login'}>Login</Link>
-        <Link href={'/register'}>Register</Link>
+        <Link href={'/auth/login'}>Login</Link>
+        <Link href={'/auth/register'}>Register</Link>
         <Image src="/next.svg" alt="test-image" width={100} height={100} />
     </>
 }
